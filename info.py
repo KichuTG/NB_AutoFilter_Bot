2d11ac37895d976bbc26d644d07fc0183ca6d17c
Kichu <kichumon333@outlook.com>
2025-06-27 13:28:58 +0530

    Update info.py

--- info.py
+++ info.py
@@ -22,7 +22,7 @@ BOT_TOKEN = environ.get('BOT_TOKEN', "")
 CACHE_TIME = int(environ.get('CACHE_TIME', 300))
 USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
-PICS = (environ.get('PICS', ' ')).split()  # Sample pic
+PICS = (environ.get('PICS', 'https://graph.org/file/bdc720faf2ff35cf92563.jpg https://envs.sh/2u4.jpg https://envs.sh/2u1.jpg')).split()  # Sample pic
 NOR_IMG = environ.get("NOR_IMG", "https://telegra.ph/file/0593a3103ba1b9a5855bf.jpg")
 MELCOW_VID = environ.get("MELCOW_VID", "")
 SPELL_IMG = environ.get("SPELL_IMG", "https://graph.org/file/a96c590ca726b2dbb9d96.jpg")
